8e2dffab267449c692c202c9fe555ce939f97c32
Blessing Mba <blessingmba3@gmail.com>
2022-03-15 18:52:44 +0000

    commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,39 +1,40 @@
 const Nav = () => {
   return (
-    <nav className="h-28 w-full bg-gray-400">
-      <div className="flex">
-        <div className="w-96">
-          <h1 className="p-12 text-center text-4xl font-bold">HIRE PURCHASE</h1>
-        </div>
-        <div className="flex w-3/4 justify-end p-12">
-          <ul className="flex">
-            <li className="mr-12">
-              <a className="text-lg font-normal text-black" href="#">
-                HOME
-              </a>
-            </li>
-            <li className="mr-12">
-              <a className="text-lg font-normal text-black" href="#">
-                PROFILE
-              </a>
-            </li>
-            <li className="mr-12">
-              <a className="text-lg font-normal text-black" href="#">
-                CONTACT US
-              </a>
-            </li>
-            <li className="mr-12">
-              <a className="text-lg font-normal text-black" href="#">
-                APPLY
-              </a>
-            </li>
-            <li className="mr-12">
-              <a className="text-lg font-normal text-black" href="#">
-                REVIEWS
-              </a>
-            </li>
-          </ul>
-        </div>
+    <nav className="w-full bg-gray-400 h-20 flex items-center">
+      <div className="w-96">
+        <h1 className="text-center text-4xl font-bold">
+          <span className="project-green-text">HIRE</span> PURCHASE
+        </h1>
+      </div>
+      <div className="flex w-3/4 justify-end items-center mr-20">
+        <ul className="flex">
+          <li className="mx-4">
+           <span className="project-green-text"><a className="text-lg font-normal" href="#">
+              HOME
+            </a>
+            </span>
+          </li>
+          <li className="mx-4">
+            <a className="text-lg font-normal text-black" href="#">
+              PROFILE
+            </a>
+          </li>
+          <li className="mx-4">
+            <a className="text-lg font-normal text-black" href="#">
+              CONTACT US
+            </a>
+          </li>
+          <li className="mx-4">
+            <a className="text-lg font-normal text-black" href="#">
+              APPLY
+            </a>
+          </li>
+          <li className="mx-4">
+            <a className="text-lg font-normal text-black" href="#">
+              REVIEWS
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
