8d321b1d702053a74678abccd4bd08f6c2c75a91
mm3eoma <jchime20@gmail.com>
2022-04-27 03:21:35 -0700

    homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Homepage from './Homepage';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <div className="">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
