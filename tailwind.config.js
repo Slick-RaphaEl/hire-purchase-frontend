fdf4d9ef02523e6ef020f3e8311abbf9f660eb48
mm3eoma <jchime20@gmail.com>
2022-04-27 13:31:04 -0700

    homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {'green-rgba': 'rgba(22,255,2,0.8)'},
+    },
   },
   plugins: [],
-};
+}
