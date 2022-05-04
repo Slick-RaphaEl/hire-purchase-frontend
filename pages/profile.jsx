867ef1824735081681694df91c0f810e6a5a8162
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2022-05-04 18:12:04 +0100

    added signIn for mobile screen

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,28 +1,60 @@
 import React from "react";
 import MainLayout from "../components/layouts/MainLayout";
 import Profile from "../components/Profile";
+import { getSession } from "next-auth/react";
 // import girl from '../img/bus-1.jpeg';
 
-const ProfilePage = () => {
-
-  const phone = +23408031344386;
-  const amount = 25000;
+const ProfilePage = ({data}) => {
 
   return (
     <MainLayout>
       <Profile 
-       heading="Ikpa Esther Uchechukwu"
-       phone={phone}
-       email="Boy@gmail/com"
-       address="22 Lion street, new heaven Enugu"
-       state="Enugu"
-       program="Tailor"
-       timeOnHirePurchase="4 months"
-       amounToBePaidThisMonth={amount}
-       timeLeftForPayment = "15 days"
+      heading= {data.name}
+       phone={data.phone}
+       email={data.email}
+       address={data.address}
+       state={data.state}
+       program={data.program}
+       timeOnHirePurchase={data.timeOnHirePurchase}
+       amountToBePaidThisMonth={data.amountToBePaidThisMonth}
+       timeLeftForPayment = {data.timeLeftForPayment}
       />
     </MainLayout>
     );
 };
 
-export default ProfilePage;
+export default ProfilePage
+
+export async function getServerSideProps(context){
+
+  const phone = +23408031344386;
+  const amount = 25000;
+
+const session = await getSession(context)
+ return{
+   props:{
+     data: session ?
+    {name:"solobachi",
+     phone:phone,
+     email:"solobachi02@gmail.com",
+     address:"No 23 Zik's drive UNN",
+     state:"Enugu",
+     program:"Web design",
+     timeOnHirePurchase:"4 months",
+     amountToBePaidThisMonth:amount,
+     timeLeftForPayment:"30 days"
+    } 
+      : 
+     {name:"username",
+     phone:phone,
+     email:"useremail@gmail.com",
+     address:"22 Lion street, new heaven Enugu",
+     state:"Lagos",
+     program:"Tailor",
+     timeOnHirePurchase:"4 months",
+     amountToBePaidThisMonth:amount,
+     timeLeftForPayment:"15 days"
+    }
+   }
+ }
+}
