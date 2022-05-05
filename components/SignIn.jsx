74acce395b22f20bcce71d4f851d644b88c754fb
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2022-05-05 18:59:15 +0100

    Edited SignIn Code

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -8,29 +8,30 @@ function SignIn(props) {
     return (
         <div className='sign-in-section w-[90%] md:w-[98%] mx-auto flex flex-col md:flex-row justify-between'>
             {/* <span className='border-2 border-red-600'>baba</span> */}
-            <div className="place-left side-img w-[80%] md:w-[50%] hidden sm:hidden lg:block relative md:mx-auto ">
+            <div className="place-left side-img w-[80%] md:w-[50%] hidden md:block relative md:mx-auto ">
                 <div className="overlay absolute opacity-60 bg-green-600 h-[100%] w-[100%] left-0 top-0 z-50" ></div>
                 <Image className='z-40' src={props.image} alt="sign in image"/>
             </div>
             <div className="place-right mx-auto my-10 md:my-36 w-[80%] md:w-[50%]">
                 <h1 className="header-text text-center pt-2 text-3xl font-medium"><span className='text-green-500'>HIRE</span> PURCHASE</h1>
-                <div className="btn-wrapper flex flex-col-reverse gap-6 pt-5 items-center">
-                 {/* Google sign in button */}
-                <button className="btn-2 border-2 border-grey-200 w-[100%] sm:w-[60%]  md:w-[50%] text-white rounded-lg bg-white">
-                <a  className="button-link  block flex flex-row justify-between  items-center" href="http://www.facebook.com/" target="_blank">
-                <FcGoogle className="bg-white text-blue-900 inline-block w-12 h-10"/>
-                <span className='bg-red-600 inline-block w-auto h-10 pt-2 sm:pt-0 sm:h-12 px-16 sm:px-12 md:px-8 md:h-10 md:pt-2 text-sm'>Continue with google
+                <div className="btn-wrapper flex flex-col gap-6 pt-5 items-center">
+                 
+                {/* facebook sign in button */}
+                <button className="btn-2 border-2 border-grey-200 text-white rounded-lg bg-white">
+                <a  className="flex items-center gap-2 block" href="http://www.facebook.com/" target="_blank">
+                <FaFacebook className="block md:text-sm h-6 w-8 text-blue-700"/>
+                <span className='bg-blue-700 block text-xs md:text-sm p-2'>Sign in with Facebook
                 </span></a>
                 </button>
-                {/* facebook sign in button */}
-                <button className="btn-2 border-2 border-grey-200 w-[100%] sm:w-[60%] md:w-[50%] text-white rounded-lg bg-white">
-                <a  className="button-link block flex flex-row justify-between  items-center" href="http://www.facebook.com/" target="_blank">
-                <FaFacebook className="bg-white text-blue-900 inline-block w-12 h-10"/>
-                <span className='bg-blue-900 inline-block w-auto h-10 sm:h-12 pt-2 sm:pt-0 px-16 sm:px-12 md:px-8 md:h-10 md:pt-2 text-sm'>Sign in with Facebook
+                {/* Google sign in button */}
+                <button className="btn-2 border-2 border-grey-200 text-white rounded-lg bg-white">
+                <a  className="button-link flex items-center gap-2 block" href="http://www.facebook.com/" target="_blank">
+                <FcGoogle className="block md:text-sm h-6 w-8 text-blue-700"/>
+                <span className='bg-red-600 block text-xs md:text-sm p-2'>Continue with google
                 </span></a>
                 </button>
                 <Link href='#'>
-                <p className="cursor-pointer text-blue-900">Don't have an account click here</p>
+                <p className="cursor-pointer text-blue-900">Don't have an account click here to login</p>
                 </Link>
                 </div>
              </div>
--- components/layouts/MainLayout.jsx
+++ components/layouts/MainLayout.jsx
@@ -11,8 +11,7 @@ export default function MainLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Lora&family=Oxygen+Mono&family=Oxygen:wght@300&family=Righteous&display=swap" rel="stylesheet" />
       </Head>
       <Nav />
-    
-      {children}
+    {children}
     </div>
   );
 }
